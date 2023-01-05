c478f06bdf837ba16c30e470c01d0757626c5315
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-05 20:52:17 -0300

    Tipos de datos en Dart

---
+++ 00-tipos_de_datos_en_dart/01-tipos_de_datos.dart
@@ -0,0 +1,26 @@
+//***** Tipos de datos en Dart *****//
+
+main()
+{
+//----Tipos de datos numericos----//
+/* Existen dos principales, los enteros y decimales */
+int numeroEntero = 10;/* Con int podemos definir variables de tipo entero */
+double numeroDecimal = 5.5;/* Con double podemos definir variables de tipo decimal */
+
+print(numeroEntero);
+print(numeroDecimal);
+
+//----Tipos de datos string----//
+String palabra = 'Santiago';/* Con String podemos definir una variable de tipo string */
+String palabra2 = "Emiliano";/* Tambien podemos usar comillas dobles */
+String multilinea = '''
+Santiago
+Emiliano
+Quiroz
+''';/* Con comillas triples podemos agregar strings multilinea */
+
+print(palabra);
+print(palabra2);
+print(multilinea);
+
+}
