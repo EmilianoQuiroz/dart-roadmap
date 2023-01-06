5d2d472b8b0e32d27c51f18979bb9096e2fb7e4e
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-05 23:28:49 -0300

    Tipos de datos en Dart

---
+++ 00-tipos_de_datos_en_dart/02-listas.dart
@@ -0,0 +1,11 @@
+//***** Listas o Arrays en Dart *****//
+main()
+{
+//Sirven para almacenar varios elementos del mismo o de distinto tipo de dato en una sola variable
+var listas = ['Elemento 1','Elemento 2', 'Elemento 3', 'Elemento 4'];
+
+listas[0] = 'Elemento 11';/* Podemos aceder y cambiar un elemento de nuestro array */
+
+print(listas);
+
+}
