0332e3bb22a7ec815f88ae48be0b42feae9385f7
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-06 01:05:06 -0300

    Sets en Dart

---
+++ 00-tipos_de_datos_en_dart/03-sets.dart
@@ -0,0 +1,12 @@
+//***** Sets en Dart *****//
+
+main()
+{
+
+var nombres = {'Pepe','Juan','Luis'};
+
+nombres.add('Jhon');/* Con .add podemos agregar otro elemento a nuestro set */
+
+print(nombres);
+
+}
