f8068df859086cf0a47c15218c91324b3198abf7
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-07 00:48:25 -0300

    Maps en Javascript

---
+++ 00-tipos_de_datos_en_dart/04-mapas.dart
@@ -0,0 +1,15 @@
+//***** Mapas en Dart ******//
+//Son objetos que internamente tienen pares clave valor
+
+main()
+{
+
+Map<String, dynamic> auto = {/* Definimos el tipo de dato de la clave y luego el tipo de dato del valor, en este caso con dynamyc este podra tomar cualquier tipo */
+  'marca' :'Toyota',
+  'puertas' :  4,
+  'color' : 'negro',
+};
+
+print(auto);/*Imprimimos el map entero */
+
+}
