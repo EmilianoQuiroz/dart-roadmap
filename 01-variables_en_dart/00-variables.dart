5c5526ec87012786c3fb9b39d171a452429ae783
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-08 21:05:06 -0300

    Variables, const y final en Dart

---
+++ 01-variables_en_dart/00-variables.dart
@@ -0,0 +1,15 @@
+//***** Variables, const y final ******//
+main()
+{
+  var a = 10;
+  final b = 10;
+  const c = 10;
+  /*A diferencia de cuando creamos variables con var, con final y const no podresmos modificar el valor de estas luego de asignarselos*/
+  /*La diferencia principal entre final y const es que final es mas ligera en memoria*/
+  /*A final y a const se les puede asignar cualquier tipo de dato */
+
+  print(a);
+  print(b);
+  print(c);
+
+}
