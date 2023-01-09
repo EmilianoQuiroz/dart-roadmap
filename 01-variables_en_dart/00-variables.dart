d21d9d0ca8a2de4c234ae97c3bbfe250e08bc097
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-08 21:09:29 -0300

    Let en Dart

--- 01-variables_en_dart/00-variables.dart
+++ 01-variables_en_dart/00-variables.dart
@@ -4,6 +4,7 @@ main()
   var a = 10;
   final b = 10;
   const c = 10;
+  late int d = 10;/*Cumple la funcion de un final pero se puede inicializar luego de su creacion, es muy usado en la creacion de clases*/
   /*A diferencia de cuando creamos variables con var, con final y const no podresmos modificar el valor de estas luego de asignarselos*/
   /*La diferencia principal entre final y const es que final es mas ligera en memoria*/
   /*A final y a const se les puede asignar cualquier tipo de dato */
@@ -11,5 +12,6 @@ main()
   print(a);
   print(b);
   print(c);
+  print(d);
 
 }
