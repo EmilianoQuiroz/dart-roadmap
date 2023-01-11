47a3f19febfb371e4d3655a7e79191645f9e1dad
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-10 23:59:17 -0300

    Otros operadores en Dart

--- 01-variables_en_dart/02-otros_operadores.dart
+++ 01-variables_en_dart/02-otros_operadores.dart
@@ -8,4 +8,15 @@ main()
   /*Si el condicional es mayor a 25 imprimimos "Es mayor a 25", sino imprimimos es menor*/
   print(respuesta);
 
+  //Operadores relacionales
+  //Todos retornan un valor booleano
+  /*
+  > Mayor que 
+  < Menor que 
+  >= Mayor o igual que
+  <= Menor o igual que
+  
+  == Revisa si dos objetos son iguales
+  != revisa si los dos objetos son diferentes
+  */
 }
