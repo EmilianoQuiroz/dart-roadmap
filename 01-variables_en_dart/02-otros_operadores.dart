44fbcb685158928e6d363e6b4ec20d2c96247fac
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-10 23:54:18 -0300

    Otros operadores en Dart

---
+++ 01-variables_en_dart/02-otros_operadores.dart
@@ -0,0 +1,11 @@
+//***** Otros Operadores ******//
+
+main()
+{
+  //Operador condicional
+  int condicional = 25;
+  String respuesta =  condicional > 20 ? 'Es mayor a 20' : 'Es menor a 20';
+  /*Si el condicional es mayor a 25 imprimimos "Es mayor a 25", sino imprimimos es menor*/
+  print(respuesta);
+
+}
